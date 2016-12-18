4e9565db9beba001c91dee10895253841c02f7e2
Bob Schockweiler <info@webfashion.in>
2016-12-18 16:01:10 +0100

    Add class files

---
+++ src/classes/ProgrammingLanguage.class.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * @author           Bob Schockweiler (https://webfashion.eu)
+ * @copyright        2016 Bob Schockweiler
+ * @license          GNU AGPLv3
+ */
+
+
+require_once "Classify.class.php";
+
+class ProgrammingLanguage extends Classify {
+	
+
+	function __construct() {
+		
+	}
+
+
+
+	/**
+	* Checks the code and returns the probability and the file extension
+	* @param string $code Code
+	* @return object
+	**/
+	public function check($code) {
+		// analyze code
+		$probabilities = $this->language($code);
+		return (object) array(
+			"probabilities" => (object) $probabilities,
+			"extension" => $this->calcPriority($probabilities),
+			);
+	}
+
+
+	/**
+	* Calculates the priority (extension) of the code
+	* @param array $probabilities Probabilites returned from Classify->language()
+	* @return string
+	**/
+	private function calcPriority($probabilities) {
+		if($this->getForcedExtension() !== false) {
+			// extension forced - use it
+			return $this->getForcedExtension();
+		}
+
+		// no extension was forced, use the extension based on the probabilities and priorities (see $this->priority())
+		$priorities = $this->priority();
+		foreach($priorities as $priority) {
+			if(isset($probabilities[$priority]) && $probabilities[$priority]) {
+				return $priority;
+			}
+		}
+
+		// anything else might be plain text?
+		return "txt";
+	}
+
+
+
+	/**
+	* Returns the priority for each language (lower index = high priority)
+	* (e.g. if HTML is used in PHP, the extension used for the file must be PHP)
+	* @return array
+	**/
+	private function priority() {
+		return array(
+			"php",
+			"xml",
+			"html",
+			"js",
+			"css",
+			"json",
+			"sql",
+		);
+	}
+
+
+}
